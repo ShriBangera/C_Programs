820a02711db1133a3297fe869ef3cf09e4b9e815
ShriBangera <120452381+ShriBangera@users.noreply.github.com>
2025-01-12 10:55:47 +0530

    Create C&F_Using#.c

---
+++ C&F_Using#.c
@@ -0,0 +1,7 @@
+//Display C and F using #
+#include<stdio.h>
+void main()
+{
+    printf("######\n#\n#\n#####\n#\n#\n#");
+    printf("\n\n   ######\n ##      ##\n#\n#\n#\n#\n#\n ##      ##\n   #####");
+}
